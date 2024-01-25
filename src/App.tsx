d0482e99700c21c05a231ff6f5d475f00728886d
Sleepingoff <justjh30@gmail.com>
2024-01-25 21:00:26 +0900

    cors router and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import SignUpPage from "./page/Signup.tsx";
+import Layout from "./components/layout.tsx";
+import Home from "./routes/home.tsx";
+import Profile from "./routes/profile.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
     <>
-      <SignUpPage />
+      <RouterProvider router={router} />
     </>
   );
 }
